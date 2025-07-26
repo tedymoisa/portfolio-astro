3e7b71f7c96a5bedbff5b619c3c7b33732f615ef
Tedy Gabriel Moisa <moisatedy@gmail.com>
2025-07-26 23:00:01 +0200

    build: add seo optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import { db } from "./src/lib/database";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: `https://${db.website}`,
+  integrations: [sitemap()],
 });
